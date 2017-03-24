e81ddcbb5618f639b84181212cf98f55d1704e08
ct.share.project <ct.share.project@gmail.com>
2017-03-24 10:34:03 +0800

    Add AsstantHelloworld Demo

---
+++ AssistantHelloworld/app/src/main/java/i19tdemo/journeyos/com/assistanthelloworld/MainActivity.java
@@ -0,0 +1,166 @@
+package i19tdemo.journeyos.com.assistanthelloworld;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.journeyui.assistantapi.BatteryInfo;
+import com.journeyui.assistantapi.Starter;
+
+public class MainActivity extends Activity  implements  View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int[] ids = {
+                R.id.startCamera_CAMERA_TYPE_FRONT,
+                R.id.startCamera_CAMERA_TYPE_REAR,
+                R.id.startFlashLight,
+                R.id.stopFlashLight,
+                R.id.startTimer,
+                R.id.startStopWatch,
+                R.id.startCalendar,
+                R.id.setScreenBrightness,
+                R.id.getBatteryInfo,
+                R.id.openAppMarket,
+        };
+
+        for (int id : ids) {
+            findViewById(id).setOnClickListener(this);
+        }
+
+        //permission check
+        if(!Settings.System.canWrite(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, 1);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // 请求码是REQUEST_CODE1，表示本次结果是申请WRITE_SETTINGS权限的结果
+        if (requestCode == 1) {
+            // 判断是否有WRITE_SETTINGS权限
+            if (Settings.System.canWrite(this)) {
+
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.startCamera_CAMERA_TYPE_FRONT:
+                startCamera_CAMERA_TYPE_FRONT();
+                break;
+            case R.id.startCamera_CAMERA_TYPE_REAR:
+                startCamera_CAMERA_TYPE_REAR();
+                break;
+            case R.id.startFlashLight:
+                startFlashLight();
+                break;
+            case R.id.stopFlashLight:
+                stopFlashLight();
+                break;
+            case R.id.startTimer:
+                startTimer();
+                break;
+            case R.id.startStopWatch:
+                startStopWatch();
+                break;
+            case R.id.startCalendar:
+                startCalendar();
+                break;
+            case R.id.setScreenBrightness:
+                setScreenBrightness();
+                break;
+            case R.id.getBatteryInfo:
+                getBatteryInfo();
+                break;
+            case R.id.openAppMarket:
+                openAppMarket();
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    @Override
+    public int checkPermission(String permission, int pid, int uid) {
+        return super.checkPermission(permission, pid, uid);
+    }
+
+    private void startCamera_CAMERA_TYPE_FRONT() {
+        Starter.startCamera(this, Starter.CAMERA_TYPE_FRONT);
+        return;
+    }
+
+    private void startCamera_CAMERA_TYPE_REAR() {
+        Starter.startCamera(this, Starter.CAMERA_TYPE_REAR);
+        return;
+    }
+
+    private void startFlashLight() {
+        Starter.startFlashLight(this);
+        return;
+    }
+
+    private void stopFlashLight() {
+        Starter.stopFlashLight(this);
+        return;
+    }
+
+    private void startTimer() {
+        String str = ((EditText) findViewById(R.id.startTimer_value)).getText().toString();
+        int seconds = Integer.parseInt(str);
+        Starter.startTimer(this,seconds);
+        return;
+    }
+
+    private void startStopWatch() {
+        String str = ((EditText) findViewById(R.id.startStopWatch_value)).getText().toString();
+        boolean enable = Integer.parseInt(str) == 0? false:true;
+        Starter.startStopWatch(this,enable);
+        return;
+    }
+
+    private void startCalendar() {
+        Starter.startCalendar(this);
+        return;
+    }
+
+    private void setScreenBrightness() {
+        String str = ((EditText) findViewById(R.id.setScreenBrightness_value)).getText().toString();
+        int brightness = Integer.parseInt(str);
+        Starter.setScreenBrightness(this, brightness);
+        return;
+    }
+
+    private void getBatteryInfo() {
+        BatteryInfo batteryInfo = Starter.getBatteryInfo(this);
+        TextView currentBatteryLevel = (TextView) findViewById(R.id.currentBatteryLevel);
+        currentBatteryLevel.setText("battery level = " + batteryInfo.currentBatteryLevel);
+
+        TextView estimatedRemainingChargingTimeSec = (TextView) findViewById(R.id.estimatedRemainingChargingTimeSec);
+        estimatedRemainingChargingTimeSec.setText("remine time = " + batteryInfo.estimatedRemainingChargingTimeSec);
+        return;
+    }
+
+    private void openAppMarket() {
+        String packageName = ((EditText) findViewById(R.id.packageName)).getText().toString();
+        Starter.openAppMarket(this,packageName);
+        return;
+    }
+}
